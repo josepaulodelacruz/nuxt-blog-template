f116cf781b8040cb682a9c492c2cf905439c3ecf
josepaulodelacruz <whoseepowlu@gmail.com>
2019-12-11 21:16:08 +0800

    feat: template for nuxt web app

---
+++ nuxt.config.js
@@ -0,0 +1,61 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'js/jquery.min.js' },
+      { src: 'js/jquery.scrolly.min.js' },
+      { src: 'js/jquery.scrollzer.min.js' },
+      { src: 'js/skel.min.js' },
+      { src: 'js/skel-layers.min.js' },
+      { src: 'js/init.js' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    extractCSS: true,
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
